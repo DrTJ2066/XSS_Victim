c0f24d63b92574379e11f239affbe26f3ea8712d
DrTJ2066 <Tj198792>
2016-03-30 14:12:11 -0500

    Initial

---
+++ XSS Victim/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace XSS_Victim
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
