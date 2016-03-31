a37f272845e306d9d003879c95de7a4c9df4fd32
DrTJ <DrTJ2066@live.com>
2016-03-31 01:04:14 -0500

    night 1

---
+++ RoyaMVC_EN/RoyaMVC_EN/Attributes/DefaultAction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace RoyaMVC.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public sealed class DefaultAction : ActionFilterAttribute 
+    {
+        public string ActionName { get; set; }
+        //public string AnonymouseActionName { get; set; }
+        //List<string> rolesList = new List<string>();
+        //public bool IsAnonymous { get; set; }
+        
+        public DefaultAction(string actionName /*, string anonymouseActionName, string appliedRoles*/) {
+            this.ActionName = actionName;
+            //this.AnonymouseActionName = anonymouseActionName;
+            //this.IsAnonymous = string.IsNullOrWhiteSpace(appliedRoles);
+            
+            //if (this.IsAnonymous == false)
+            //    this.rolesList = appliedRoles.Replace(" ", "").Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            //var isUserInDefinedRoles = IsInDefinedRoles(filterContext);
+            if (string.IsNullOrWhiteSpace(filterContext.ActionDescriptor.ActionName)) {
+                filterContext.HttpContext.Response.Redirect("/" + filterContext.ActionDescriptor.ControllerDescriptor.ControllerName + "/" + this.ActionName);
+            }
+
+            //filterContext.ActionDescriptor.ActionName
+        }
+
+        //public bool IsInDefinedRoles(ActionExecutingContext filterContext) {
+        //    if (this.rolesList.Count > 0) {
+        //        foreach (var item in this.rolesList)
+        //            if (filterContext.HttpContext.User.IsInRole(item))
+        //                return true;
+        //    }
+
+        //    return false;
+        //}
+    }
+}
